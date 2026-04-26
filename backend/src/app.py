bea29bc56947a082db9254cdc4242b077a6e613d
Kathya <kr569@cornell.edu>
2026-04-26 19:33:41 -0400

    add ed backend folder

---
+++ backend/src/app.py
@@ -0,0 +1,33 @@
+import json
+
+from db import db, Task, Subtask, Category
+from flask import Flask, request
+
+# define db filename
+db_filename = "todo.db"
+app = Flask(__name__)
+
+# setup config
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_filename}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+# initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
